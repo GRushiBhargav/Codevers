971f6afdd3c3a71934e689418abdcc6977150e09
GRushiBhargav <grushibhargav@gmail.com>
2024-01-02 22:49:24 +0530

    Logout toast reset password

--- src/components/Modals/ResetPassword.tsx
+++ src/components/Modals/ResetPassword.tsx
@@ -1,4 +1,7 @@
-import React, { FormEvent } from 'react';
+import { auth } from '@/firebase/firebase';
+import React, { FormEvent, useEffect, useState } from 'react';
+import { useSendPasswordResetEmail } from 'react-firebase-hooks/auth';
+import { toast } from 'react-toastify';
 
 type ResetPasswordProps = {
     
@@ -6,16 +9,23 @@ type ResetPasswordProps = {
 
 const ResetPassword:React.FC<ResetPasswordProps> = () => {
     
-    function handleReset(event: FormEvent<HTMLFormElement>): void {
-        throw new Error('Function not implemented.');
-    }
+	const [email, setEmail] = useState("");
+	const [sendPasswordResetEmail, sending, error] = useSendPasswordResetEmail(auth);
+    const handleReset = async (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		const success = await sendPasswordResetEmail(email);
+		if (success) {
+			toast.success("Password reset email sent", { position: "top-center", autoClose: 3000,theme: "colored"});
+		}
+	};
 
-    function setEmail(value: string): void {
-        throw new Error('Function not implemented.');
-    }
-
+	useEffect(() => {
+		if (error) {
+			alert(error.message);
+		}
+	}, [error]);   
     return  (
-		<form className='space-y-6 px-6 lg:px-8 pb-4 sm:pb-6 xl:pb-8' onSubmit={handleReset}>
+		<form className='space-y-6 px-6 lg:px-8 pb-4 sm:pb-6 xl:pb-8' onSubmit={handleReset} >
 			<h3 className='text-xl font-medium  text-white'>Reset Password</h3>
 			<p className='text-sm text-white '>
 				Forgotten your password? Enter your e-mail address below, and we&apos;ll send you an e-mail allowing you
